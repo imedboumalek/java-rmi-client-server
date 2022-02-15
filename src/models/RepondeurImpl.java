09ed7d3bfa257269ed23bba3e9451347e9b75b5f
Imed Boumalek <imedboumalek@gmail.com>
2022-02-15 08:22:53 +0100

    fixes

--- src/models/RepondeurImpl.java
+++ src/models/RepondeurImpl.java
@@ -6,6 +6,7 @@ public class RepondeurImpl implements Repondeur {
 
     @Override
     public String repondre(String name) throws RemoteException {
+        System.out.println("RepondeurImpl.repondre() => " + name);
         return "Hello " + name;
     }
 
